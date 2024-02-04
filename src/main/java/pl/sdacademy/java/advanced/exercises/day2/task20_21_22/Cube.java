2ad517f2101e6c8f33051baaa4ed4700ba91bfab
Pawel Dudko <paweldudko@outlook.com>
2024-02-04 15:31:17 +0100

    solution for task22

--- src/main/java/pl/sdacademy/java/advanced/exercises/day2/task20_21_22/Cube.java
+++ src/main/java/pl/sdacademy/java/advanced/exercises/day2/task20_21_22/Cube.java
@@ -1,6 +1,6 @@
 package pl.sdacademy.java.advanced.exercises.day2.task20_21_22;
 
-public class Cube extends Shape3D{
+public class Cube extends Shape3D implements Fillable{
     private final int a;
 
     public Cube(int a) {
@@ -15,5 +15,24 @@ public class Cube extends Shape3D{
     @Override
     int calculatePerimeter() {
         return 12 * a;
+    }
+
+    /**
+     * opsiać co metoda robi
+     * @param volume - czym jest ten param
+     * @return
+     *  0 -> oznacza, że figura jest napełniona po brzegi
+     * +1 -> oznacza, że wody jest zbyt dużo
+     * -1 -> oznacza, że wody jest zbyt mało
+     */
+    @Override
+    public int fill(double volume) {
+        double maxVolume = calculateVolume();
+        if(maxVolume == volume) {
+            return 0;
+        } else if(maxVolume > volume) {
+            return -1;
+        }
+        return 1;
     }
 }
--- src/main/java/pl/sdacademy/java/advanced/exercises/day2/task20_21_22/Task20_21_22.java
+++ src/main/java/pl/sdacademy/java/advanced/exercises/day2/task20_21_22/Task20_21_22.java
@@ -15,17 +15,18 @@ public class Task20_21_22 {
         int hexagonPerimeter = hexagon.calculatePerimeter();
         System.out.println("hexagonPerimeter: " + hexagonPerimeter); //12
 
-        Shape3D cube = new Cube(2);
+        Cube cube = new Cube(2);
         double cubeVolume = cube.calculateVolume();
         int cubePerimeter = cube.calculatePerimeter();
         System.out.println("cubeVolume: " + cubeVolume); //8
         System.out.println("cubePerimeter: " + cubePerimeter); //24
+
+        System.out.println(cube.fill(7));
 
 //        Shape cube = new Cube(2);
 //        double cubeVolume = ((Cube) cube).calculateVolume();
 //        int cubePerimeter = cube.calculatePerimeter();
 //        System.out.println("cubeVolume: " + cubeVolume); //8
 //        System.out.println("cubePerimeter: " + cubePerimeter); //24
-
     }
 }

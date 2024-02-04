0563235c3f4987bb1dfb0a8cb3036c4f635aa37c
Pawel Dudko <paweldudko@outlook.com>
2024-02-04 10:03:59 +0100

    solution for task12

---
+++ src/main/java/pl/sdacademy/java/advanced/exercises/day2/task12/Manufacturer.java
@@ -0,0 +1,40 @@
+package pl.sdacademy.java.advanced.exercises.day2.task12;
+
+import java.util.Objects;
+
+public class Manufacturer {
+    private final String name;
+    private final int foundedYear;
+    private final String country;
+
+    public Manufacturer(String name, int foundedYear, String country) {
+        this.name = name;
+        this.foundedYear = foundedYear;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getFoundedYear() {
+        return foundedYear;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        Manufacturer that = (Manufacturer) o;
+        return foundedYear == that.foundedYear && Objects.equals(name, that.name) && Objects.equals(country, that.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, foundedYear, country);
+    }
+}
